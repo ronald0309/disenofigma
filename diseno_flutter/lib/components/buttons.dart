7c2fd6cbeb94db01c474eb0c06726ec19ea9d81d
ronald0309 <42876234+ronald0309@users.noreply.github.com>
2023-05-02 23:32:25 -0600

    Witged botones grandes

--- diseno_flutter/lib/components/buttons.dart
+++ diseno_flutter/lib/components/buttons.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
 
+class ButtonAccion extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color color;
+  final Size size; // Nueva propiedad para definir el tamaño del botón
+  final Color textColor;
+  const ButtonAccion({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    required this.color,
+    required this.size, // Agregamos size al constructor
+    required this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size.width,
+      height: size.height,
+      child: Center(
+          child: TextButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: this.textColor,
+            fontSize: 16.0,
+          ),
+        ),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+          padding: MaterialStateProperty.all<EdgeInsets>(
+            const EdgeInsets.symmetric(
+              vertical: 16.0,
+              horizontal: 24.0,
+            ),
+          ),
+          shape: MaterialStateProperty.all<OutlinedBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(32.0),
+            ),
+          ),
+          minimumSize: MaterialStateProperty.all<Size>(
+              size), // Definimos el tamaño mínimo
+        ),
+      )),
+    );
+  }
+}
